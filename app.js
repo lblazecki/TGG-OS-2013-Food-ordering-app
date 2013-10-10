04570cdd8e779a7f15015fd49634a9dbb69ac87e
Luka Blazecki <Luka.Blazecki@infobip.com>
2013-10-10 20:58:18 +0200

    Get channels

--- app.js
+++ app.js
@@ -13,14 +13,22 @@ app.post('/user/:userID/order', function (req, res) {
     manageOrder(req.params.userID, req.body);
     res.send(204, null);
 });
+app.get('/user/:userID/allAvailableChannels', function (req, res) {
+    getAllAvailableChannels(function (code, channels) {
+        res.send(code, channels);
+    });
+});
 app.post('/sendOrder', function (req, res) {
     sendOrder(req.body, function (code, returnObject) {
         res.send(code, returnObject);
     });
 });
 
+
 app.listen(3000);
 console.log("Server up and listening on port 3000");
+
+
 
 function manageOrder(userID, order) {
     console.log('User with ' + userID + ', ordered : ' + JSON.stringify(order));
@@ -44,6 +52,22 @@ function sendOrder(order, callback) {
         json : true,
         headers : {Authorization : pushAuthorization},
         body : messageBody
+    };
+    request(options, function (error, response, body) {
+        if (error || response.statusCode  !== 200) {
+            callback(200, body);
+            return;
+        }
+        callback(500, body);
+    });
+}
+
+function getAllAvailableChannels(callback) {
+    var options = {
+        url : 'https://pushapi.infobip.com/1/application/' + applicationID + '/channels',
+        method : 'GET',
+        json : true,
+        headers : {Authorization : pushAuthorization}
     };
     request(options, function (error, response, body) {
         if (error || response.statusCode  !== 200) {
