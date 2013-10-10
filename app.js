e0bb2a2192a7835c686f6ad551e7c316f261450a
Luka Blazecki <Luka.Blazecki@infobip.com>
2013-10-10 17:57:23 +0200

    First api call

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
-app.get('/hello', function(req, res) {
+
+app.use(express.bodyParser());
+app.get('/hello', function (req, res) {
     res.send({message: 'Hello World'});
 });
+app.post('/user/:userID/order', function (req, res) {
+    manageOrder(req.params.userID, req.body);
+    res.send(204, null);
+});
+
 app.listen(3000);
+console.log("Server up and listening on port 3000");
+
+function manageOrder(userID, order) {
+    console.log('User with ' + userID + ', ordered : ' + JSON.stringify(order));
+}
+
 
