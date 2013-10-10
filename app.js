d682a9180b801cd392706f6bdcd398762939e9ff
Luka Blazecki <Luka.Blazecki@infobip.com>
2013-10-10 17:59:09 +0200

    First server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
-app.get('/hello', function(req, res) {
+app.get('/hello', function (req, res) {
     res.send({message: 'Hello World'});
 });
+
 app.listen(3000);
+console.log("Server up and listening on port 3000");
 
