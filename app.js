99ae17f3f007d2a041f0d86637016d30caaad121
Luka Blazecki <Luka.Blazecki@infobip.com>
2013-10-13 10:04:44 +0200

    Auth fixed

--- app.js
+++ app.js
@@ -2,49 +2,46 @@ var express             = require('express');
 var request             = require('request');
 var app = express();
 
-var applicationID       = '0607c3f5d047';
-var pushAuthorization   = 'Basic dGdnMjAxMzp0Z2cyMDEz';
+var applicationID       = '868e545dffae';
+var pushAuthorization   = 'Basic cHVzaGRlbW86dGdnMjAxMw==';
 
 app.use(express.bodyParser());
 app.get('/hello', function (req, res) {
     res.send({message: 'Hello World'});
 });
-app.post('/user/:userID/order', function (req, res) {
-    manageOrder(req.params.userID, req.body);
-    res.send(204, null);
-});
-app.get('/user/:userID/allAvailableChannels', function (req, res) {
+app.get('/allAvailableChannels', function (req, res) {
     getAllAvailableChannels(function (code, channels) {
         res.send(code, channels);
     });
 });
-app.post('/sendOrder', function (req, res) {
-    sendOrder(req.body, function (code, returnObject) {
+app.post('/user/:userID/order', function (req, res) {
+    manageOrder(req.params.userID, req.body);
+    res.send(204, null);
+});
+app.post('/sendOffer', function (req, res) {
+    sendOffer(req.body, function (code, returnObject) {
         res.send(code, returnObject);
     });
 });
 
-
 app.listen(3000);
 console.log("Server up and listening on port 3000");
-
-
 
 function manageOrder(userID, order) {
     console.log('User with ' + userID + ', ordered : ' + JSON.stringify(order));
 }
 
-function sendOrder(order, callback) {
+function sendOffer(order, callback) {
 
     var scheduleTime = new Date("2013-10-13 17:30:00").getTime();
     var messageBody = {
         sentType : "channels",
         mimeType : "text/plain",
         OSTypes : ["Android"],
-        channelNames : ['Osijek'],
-        androidData : {},
+        channelNames : ['Kod Ruže'],
         expiryOffset : 6 * 60 * 60,
         scheduleTime : scheduleTime,
+        androidData : {},
         notificationMessage : JSON.stringify(order)
     };
     var options = {
@@ -55,13 +52,14 @@ function sendOrder(order, callback) {
         body : messageBody
     };
     request(options, function (error, response, body) {
-        if (error || response.statusCode  !== 200) {
-            callback(200, body);
+        if (error || response.statusCode !== 200) {
+            callback(500, body);
             return;
         }
-        callback(500, body);
+        callback(200, body);
     });
 }
+
 
 function getAllAvailableChannels(callback) {
     var options = {
